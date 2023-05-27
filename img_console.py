f512ba1279bd4a22d6d6c75bcbc5a29f3cc16f31
rounak <rounakkole@gmail.com>
2023-05-27 15:17:10 +0530

    img_console v2

--- img_console.py
+++ img_console.py
@@ -2,7 +2,6 @@ def img_console():
     from PIL import Image
     
     width = 50
-    step = 20
     img = Image.open('corner_border.jpg')
     
     pix1, pix2 = img.size
@@ -22,14 +21,10 @@ def img_console():
         while (i < (pixo1 - 1)):
             i = i + 1
             ip_rgb = (gray_img.load()[i, j]) #00 to 255
-            for k in range(14):
-                ip_window = (k * step)
-                if(abs(ip_rgb - ip_window) <= (step / 2)):
-                    if(ip_window >= 160):
-                        print(end = "  ")
-                    elif(ip_window >= 100):
-                        print(end = "0 ")
-                    else:
-                        print(end = "00")
-                    break
-            
+
+            if(ip_rgb >= 160):
+                print(end = "  ")
+            elif(ip_rgb >= 100):
+                print(end = "0 ")
+            else:
+                print(end = "00")
